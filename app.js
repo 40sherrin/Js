406383660d7b0de3feff42d8804997cdf02a13ea
40sherrin <fareedaskari42@gmail.com>
2021-03-17 11:42:37 +1100

    express package installed

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  if(req.url === '/'){
-    res.write('Welcome back to home')
-    res.end()
-  }else if(req.url === '/api/courses'){
-    res.write(JSON.stringify([1,2,3,4]))
-    res.end()
-  }
-})
-
-server.listen(8080)
+const express = require('express');
