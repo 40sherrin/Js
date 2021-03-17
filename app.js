3f2293b43c270a209e06cb40a1f55eec56d54a0c
40sherrin <fareedaskari42@gmail.com>
2021-03-17 11:38:10 +1100

    Creating routes

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   if(req.url === '/'){
-    res.end('Welcome to home')
+    res.write('Welcome back to home')
+    res.end()
+  }else if(req.url === '/api/courses'){
+    res.write(JSON.stringify([1,2,3,4]))
+    res.end()
   }
 })
 
